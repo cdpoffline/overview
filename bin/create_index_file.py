e0fb8f8ae04ec918c97751cbb3572dd8e86749b9
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-30 18:48:07 +0000

    added categories of materials

--- bin/create_index_file.py
+++ bin/create_index_file.py
@@ -18,16 +18,26 @@ template_path = os.path.join(this_directory, TEMPLATE_FILE)
 index_path = os.path.join(this_directory, INDEX_FILE)
 
 # collect variables
-materials = {}
-variables = dict(materials = materials)
+variables = {}
+
+def update_variables(update, variables = variables):
+    if isinstance(update, dict):
+        for name in update:
+            if name not in variables or not update_variables(update[name], variables[name]):
+                variables[name] = update[name]
+        return True
+    return False
+
+def add_configuration_file(configuration_filename):
+    with open(configuration_filename) as configuration_file:
+        module_variables = yaml.load(configuration_file)
+    update_variables(module_variables)
 
 for module_name in module_names:
     module_path = os.path.join(modules_path, module_name)
     configuration_filename = os.path.join(module_path, CONFIGURATION_FILENAME)
     if os.path.isfile(configuration_filename):
-        with open(configuration_filename) as configuration_file:
-            module_variables = yaml.load(configuration_file)
-        materials[module_name] = module_variables
+        add_configuration_file(configuration_filename)
 
 pprint.pprint(variables)
 
@@ -37,7 +47,7 @@ with open(template_path) as template_file:
 
 template = Template(template_text)
 with open(index_path, "w") as index_file:
-    index_file.write(template.render(variables))
+    index_file.write(template.render(variables).encode("UTF-8"))
 
 
 
