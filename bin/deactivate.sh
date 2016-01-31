f2c5ef380eb7be1294aa4f635cfb1076368d7a70
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-31 16:14:11 +0000

    added overview link to offline materials

---
+++ bin/deactivate.sh
@@ -0,0 +1,3 @@
+cd "`dirname \"$0\"`"
+
+rm ../web/offline-materials/index.html
--- bin/update.sh
+++ bin/update.sh
@@ -3,3 +3,14 @@
 cd "`dirname $0`"
 
 ./create_index_file.py "`pwd`"
+
+echo "
+<html>
+  <head>
+    <meta http-equiv=refresh content='0; url=overview' />
+  </head>
+  <body>
+    <h1><a href="overview">overview</a></h1>
+  </body>
+</html>
+" > ../web/offline-materials/index.html
