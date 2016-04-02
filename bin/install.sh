0db2c93651bbd6e3845fbd0092e1d44e4952fb9b
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-04-02 00:10:51 -0700

    using pip2

--- bin/install.sh
+++ bin/install.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get -y -q install realpath python python-pip
 
-sudo pip install --upgrade pip
-sudo pip install --upgrade Jinja2 PyYAML
+sudo pip2 install --upgrade pip
+sudo pip2 install --upgrade Jinja2 PyYAML
